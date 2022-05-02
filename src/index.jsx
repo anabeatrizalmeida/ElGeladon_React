2ccde5f8a1ef15428ef342f47147c88778e7eecd
anabeatrizalmeida <ana-almeida@hotmail.com>
2022-05-02 12:54:11 -0300

    changes

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,15 @@
-import React from "react"; /*importe da biblioteca react*/
-import ReactDOM from "react-dom"; /*renderiza documentos em tela (importa) - biblioteca reactDOM*/
-import Home from "./components/Home"; /*importe componentes*/
-import "./index.css";
+import React from "react"; 
+import ReactDOM from "react-dom"; 
+import Home from "./views/Home/Home"; 
+import "assets/styles/main.css";
 
 
 ReactDOM.render(
-  /*renderizando a biblioteca reactDOM - renderiza o que está dentro do parenteses*/
+  
   <React.StrictMode>
     <Home />
-  </React.StrictMode> /* alerta para problemas na documentação*/,
+  </React.StrictMode> ,
   document.getElementById(
     "root"
-  ) /* no index.html no body tem uma div com id chamado root, é lá que os componentes são renderizados. Aqui é onde aponta que isso irá acontecer*/
+  ) 
 );
